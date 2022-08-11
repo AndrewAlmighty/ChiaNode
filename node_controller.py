cda7d32b8a6b2f301a6de9496607bc1de3483e6a
AndrewAlmighty <xaidianx@gmail.com>
2022-08-11 18:49:05 +0200

    Update repo after adding new disk

--- node_controller.py
+++ node_controller.py
@@ -12,15 +12,22 @@ import subprocess
 import time
 
 # GLOBAL VARIABLES SECTION
+FARMER_VALID_PLOTS_COUNT = 862
+
 LED_CTRL = LED(17)
 
 BREAK_BETWEEN_JOBS_IN_SECONDS = 180
+WALLET_DATA_STORE_INTERVAL = 86400 # once per day
+
+WALLET_LOG_PATH="/home/raspberry/wallet.log"
 
 CONTROLLER_LOG_PATH="/home/raspberry/controller.log"
 DISKS_JSON_FILE_PATH = "/home/raspberry/controller/disks.json"
 
 RPC_REQUEST_HEADERS = {'Content-Type': 'application/json'}
 BLOCKCHAIN_STATE_URL = "https://localhost:8555/get_blockchain_state"
+FARMER_STATE_URL = "https://localhost:8559/get_harvesters_summary"
+WALLET_BALANCE_URL = "https://localhost:9256/get_wallet_balance"
 CHIA_ROOT_DIR = os.getenv("CHIA_ROOT")  # shouldn't have '/' in the end.
 FULL_NODE_CERT = (CHIA_ROOT_DIR + '/config/ssl/full_node/private_full_node.crt', CHIA_ROOT_DIR + '/config/ssl/full_node/private_full_node.key')
 
@@ -32,6 +39,9 @@ ALL_DISKS_CONNECTED = True
 NETWORK_WORKS = True
 NODE_SYNCED = True
 CHIA_NODE_ENABLED = True
+FARMER_SYNCED = True
+FARMER_PLOTS_NUMBER_GOOD = True
+
 
 # END OF GLOBAL VARIABLES SECTION
 
@@ -49,12 +59,11 @@ def handleSigTerm(signalNumber, frame):
 
 class Logger:
 
-    def __init__(self, controller_log_path):
-        #self.wallet_log_path = wallet_log_path
-        self.controller_log_path = controller_log_path
-        #self.wallet_log("Controller started work")
+    def __init__(self):
+        self.wallet_log_path = WALLET_LOG_PATH
+        self.controller_log_path = CONTROLLER_LOG_PATH
+        self.wallet_log("Controller started work")
         self.controller_log("Controller started work")
-        print("Logger prepared ...")
 
     def __log(self, file, log):
         log_file = open(file, "a")
@@ -63,23 +72,22 @@ class Logger:
         log_file.write("[" + dt_string + "]: " + log + "\n")
         log_file.close()
         
-    #def wallet_log(self, log):
-    #    wallet_log_file = open(self.wallet_log_path, "a")
-    #    self.__log(self.wallet_log_path, log)
+    def wallet_log(self, log):
+        self.__log(self.wallet_log_path, log)
 
     def controller_log(self, log):
-        #wallet_log_file = open(self.wallet_log_path, "a")
         self.__log(self.controller_log_path, log)
 
 # --- END OF LOGGER
 
 class Controller:
 
-    # {"disk_uuid": {"mount_point", "name", "is_mounted"}}
+    # {"disk_uuid": {"mount_point", "is_mounted"}}
     disks_mapping = {}
 
     def __init__(self, disks_mapping_file):
-        self.logger = Logger(CONTROLLER_LOG_PATH)
+        self.previous_confirmed_amount = 0;
+        self.logger = Logger()
         self.disks_mapping_file = disks_mapping_file
 
     def __load_disks_mapping(self):
@@ -94,15 +102,13 @@ class Controller:
             loaded_disk_ids.append(disk_id)
 
             if disk_id not in self.disks_mapping.keys():
-                disk_params = {"name": disk_json["name"], "mount_point": disk_json["mount_point"], "is_mounted": False}
+                disk_params = {"mount_point": disk_json["mount_point"], "is_mounted": False}
                 self.disks_mapping[disk_id] = disk_params
                 mapping_updated = True
                 self.__umount_disk(disk_id, disk_json["mount_point"])
             else:
                 disk_params = self.disks_mapping[disk_id]
-                if disk_params["name"] != disk_json["name"]:
-                    self.disks_mapping[disk_id]["name"] = disk_json["name"]
-                    mapping_updated = True
+
                 if disk_params["mount_point"] != disk_json["mount_point"]:
                     self.disks_mapping[disk_id]["mount_point"] = disk_json["mount_point"]
                     self.__umount_disk(disk_id, disk_params["mount_point"])
@@ -145,8 +151,8 @@ class Controller:
             self.logger.controller_log("Unmounting " + mount_point + ". Found " + str(filesystems_mounted_count) + " filesystems mounted there.")
             for i in range(0, filesystems_mounted_count):
               umount(mount_point)
-        except:
-            self.logger.controller_log("Unmounting " + mount_point + " failed.")
+        except Exception as e:
+            self.logger.controller_log("Unmounting " + mount_point + " failed. Error: " + str(e))
 
     # - end of umount disk impl
 
@@ -162,18 +168,13 @@ class Controller:
             return
 
         try:
-            ls("/dev/disk/by-uuid/" + disk_id)
-        except:
-            self.logger.controller_log("Mounting " + disk_id + " to " + mount_point + " failed. Disk is not connected!")
-            return
-
-        try:
             mount("UUID="+ disk_id, mount_point)
             self.disks_mapping[disk_id]["is_mounted"] = True
             plots_count = len(glob.glob1(mount_point, "*.plot"))
             self.logger.controller_log("Mounted " + disk_id + ". Mount point: " + mount_point + ". Plots count: " + str(plots_count))
-        except:
-            self.logger.controller_log("Mounting " + disk_id + " to " + mount_point + " failed.")
+            time.sleep(3)
+        except Exception as e:
+            self.logger.controller_log("Mounting " + disk_id + " to " + mount_point + " failed. Error: " + str(e))
 
     # - end of mount disk impl
 
@@ -183,30 +184,54 @@ class Controller:
 
         for disk_id, disk_params in self.disks_mapping.items():
             mount_point = disk_params["mount_point"]
-            files_cnt = 0
-            if disk_params["is_mounted"]:
+            disk_mounted = disk_params["is_mounted"]
+
+            if disk_mounted:
+                files_cnt = 0
                 try:
                     files_cnt = len(os.listdir(mount_point))
-                except:
-                    pass
+                except Exception as e:
+                    self.logger.controller_log("Cannot check the content of mount point '" + mount_point + "'. Error: " + str(e))
+                    any_problems = True
+                    continue
+
                 if files_cnt == 0:
                     self.__umount_disk(disk_id, mount_point)
+                    disk_mounted = False
                     any_problems = True
-            else:
-                if len(os.listdir(mount_point)) != 0:
-                    self.logger.controller_log("Cannot mount disk " + disk_id + " to " + mount_point + " because point point contains files.")
+
+            if not disk_mounted:
+                files_cnt = 0
+                try:
+                    files_cnt = len(os.listdir(mount_point))
+                except Exception as e:
+                    self.logger.controller_log("Cannot check the content of mount point '" + mount_point + "'. Error: " + str(e))
+                    any_problems = True
+                    continue
+  
+                if files_cnt != 0:
+                    self.logger.controller_log("Cannot mount disk " + disk_id + " to " + mount_point + " because mount point contains files.")
                 else:
                     self.__mount_disk(disk_id, mount_point)
                     try:
                         files_cnt = len(os.listdir(mount_point))
-                    except:
-                        pass
+                    except Exception as e:
+                        self.logger.controller_log("Cannot check the content of mount point '" + mount_point + "'. Error: " + str(e))
+                        any_problems = True
+                        continue
+
                     if files_cnt == 0:
                         any_problems = True
 
         if not any_problems:
+            if not ALL_DISKS_CONNECTED:
+                self.logger.controller_log("All disks are connected and mounted again!")
+
             ALL_DISKS_CONNECTED = True
         else:
+            if ALL_DISKS_CONNECTED:
+                self.logger.controller_log("Some disks are disconnected!")
+
             ALL_DISKS_CONNECTED = False
 
     # - end of check mount points impl
@@ -238,15 +263,16 @@ class Controller:
             elif is_synced and not NODE_SYNCED:
                 NODE_SYNCED = True
                 self.logger.controller_log("Node is synchronized with blockchain.")
+
         except requests.ConnectionError as error:
             NODE_SYNCED = False
             self.logger.controller_log("Cannot send request to full node to check if blockchain is synced. Connection error: " + str(error))
         except requests.HTTPError as error:
             NODE_SYNCED = False
             self.logger.controller_log("Cannot send request to full node to check if blockchain is synced. Http error: " + str(error))
-        except:
+        except Exception as e:
             NODE_SYNCED = False
-            self.logger.controller_log("Cannot send request to full node to check if blockchain is synced. (Not known error)")
+            self.logger.controller_log("Cannot send request to full node to check if blockchain is synced. " + str(e))
 
     # - end of check blockchain sync impl
 
@@ -275,22 +301,73 @@ class Controller:
         global CHIA_NODE_ENABLED
         global ALL_DISKS_CONNECTED
         global LED_CTRL
+        global FARMER_PLOTS_NUMBER_GOOD
+        global FARMER_SYNCED
 
-        if not NETWORK_WORKS:
-            LED_CTRL.on()
-        elif not NODE_SYNCED:
-            LED_CTRL.on()
-        elif not CHIA_NODE_ENABLED:
-            LED_CTRL.on()
-        elif not ALL_DISKS_CONNECTED:
+        if not NETWORK_WORKS or not NODE_SYNCED or not CHIA_NODE_ENABLED or not ALL_DISKS_CONNECTED or not FARMER_SYNCED or not FARMER_PLOTS_NUMBER_GOOD:
             LED_CTRL.on()
         else:
             LED_CTRL.off()
+    
+    # - end of notify if problem
+
+    def __check_farmer(self):
+        try:
+            response = json.loads(requests.post(FARMER_STATE_URL, data='{}', headers=RPC_REQUEST_HEADERS, cert=FULL_NODE_CERT, verify=False).text)
+            harvester = response["harvesters"][0]
+            global FARMER_SYNCED
+            global FARMER_PLOTS_NUMBER_GOOD
+
+            if FARMER_SYNCED and harvester["syncing"] is not None:
+                self.logger.controller_log("Farmer is not synchronized with blockchain.")
+                FARMER_SYNCED = False
+            elif not FARMER_SYNCED and harvester["syncing"] is None:
+                self.logger.controller_log("Farmer is synchronized with blockchain.")
+                FARMER_SYNCED = True
+
+            if FARMER_PLOTS_NUMBER_GOOD and harvester["plots"] != FARMER_VALID_PLOTS_COUNT:
+                self.logger.controller_log("Farmer's plots count is not good. Plots count: " + str(harvester["plots"]) + ", expected: " + str(FARMER_VALID_PLOTS_COUNT))
+                FARMER_PLOTS_NUMBER_GOOD = False
+            elif not FARMER_PLOTS_NUMBER_GOOD and harvester["plots"] == FARMER_VALID_PLOTS_COUNT:
+                self.logger.controller_log("Farmer's plots count is good again.")
+                FARMER_PLOTS_NUMBER_GOOD = True
+
+        except requests.ConnectionError as error:
+            FARMER_SYNCED = False
+            self.logger.controller_log("Cannot send request to full node to check if farmer is synced. Connection error: " + str(error))
+        except requests.HTTPError as error:
+            FARMER_SYNCED = False
+            self.logger.controller_log("Cannot send request to full node to check if farmer is synced. Http error: " + str(error))
+        except Exception as e:
+            FARMER_SYNCED = False
+            self.logger.controller_log("Cannot send request to full node to check if farmer is synced. " + str(e))
+
+    # - check farmer plots count
+
+    def __store_wallet_data(self):
+        try:
+            response = json.loads(requests.post(WALLET_BALANCE_URL, data='{"wallet_id":1}', headers=RPC_REQUEST_HEADERS, cert=FULL_NODE_CERT, verify=False).text)
+            current_confirmed_amount = response["wallet_balance"]["confirmed_wallet_balance"]
+            diff_in_mojos = current_confirmed_amount - self.previous_confirmed_amount
+            MOJO_TO_CHIA_DIVIDER = 1000000000000
+            diff_in_chia = diff_in_mojos / MOJO_TO_CHIA_DIVIDER
+            self.previous_confirmed_amount = current_confirmed_amount
+            self.logger.wallet_log("Wallet 1 amounts - confirmed: " + str(current_confirmed_amount) + " (diff: " + str(round(diff_in_chia, 4)) + " xch, " + str(diff_in_mojos) + " mojos), spendable: " + str(response["wallet_balance"]["spendable_balance"]))
+
+        except Exception as e:
+            self.logger.wallet_log("Unable to get wallet's balance. " + str(e))
+
+    # - store wallet data
 
     def run(self):
         global CONTROLLER_ENABLED
-        global THREAD_SLEEP_IN_SECONDS
+        global BREAK_BETWEEN_JOBS_IN_SECONDS
+        global WALLET_DATA_STORE_INTERVAL
+        global NODE_SYNCED
+        global CHIA_NODE_ENABLED
+
         last_time = 0
+        wallet_last_time_check = 0
         while CONTROLLER_ENABLED:
             now = time.time()
             if now - last_time > BREAK_BETWEEN_JOBS_IN_SECONDS:
@@ -298,15 +375,25 @@ class Controller:
                 self.__check_mount_points()
                 self.__is_process_alive()
                 self.__check_network()
-                self.__check_blockchain_sync()
-                self.__notify_if_problem()
+
+                if CHIA_NODE_ENABLED:
+                    self.__check_blockchain_sync()
+                    self.__check_farmer()
+
+                if NODE_SYNCED and now - wallet_last_time_check > WALLET_DATA_STORE_INTERVAL:
+                  self.__store_wallet_data()
+                  wallet_last_time_check = now
+
                 last_time = time.time()
+                self.__notify_if_problem()
+
             time.sleep(10)
 
         self.logger.controller_log("Stopping " + CHIA_NODE_PROCESS_NAME + " ...")
         output = subprocess.check_output(PATH_TO_RUN_CHIA_SCRIPT, shell=True)
         self.logger.controller_log(CHIA_NODE_PROCESS_NAME + " stopped. Output:\n" + str(output))
         self.logger.controller_log("Controller is stopped with sigint or sigterm")
+        self.logger.wallet_log("Controller is stopped with sigint or sigterm")
 
 # --- END OF CONTROLLER
 
